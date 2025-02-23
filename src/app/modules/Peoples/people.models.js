d6659b1e7b464113cc1205d1877b1a99d9e5956d
sheikhsohag <sheikhsohag000@gmail.com>
2025-02-23 06:55:54 +0600

    role added in user

--- src/app/modules/Peoples/people.models.js
+++ src/app/modules/Peoples/people.models.js
@@ -1,10 +1,26 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'restaurant', 'user'], default: 'user' }
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew) { 
+    const User = mongoose.model('User');
+    const count = await User.estimatedDocumentCount();
+
+    if (count === 0) {
+      this.role = 'admin';
+    }
+  }
+  next();
+});
 
 
 const User = mongoose.model('User', userSchema);
